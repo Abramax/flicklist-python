48c7aa8c9c32e11c5b34b12e86102904d502cf7b
Sean Riha <swriha@gmail.com>
2017-01-19 20:44:55 -0600

    FlickList Studio 1 Complete

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movieList = ["Indiana Jones", "The Matrix", "The Big Lebowski", "Big", "Tootsie"]
 
         # TODO: randomly choose one of the movies, and return it
 
-        return "The Big Lebowski"
+        return random.choice(movieList)
 
     def get(self):
         # choose a movie by invoking our new function
         movie = self.getRandomMovie()
+        movie2 = self.getRandomMovie()
+
+        while movie2 == movie:
+            movie2 = self.getRandomMovie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
@@ -21,7 +27,11 @@ class Index(webapp2.RequestHandler):
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
 
+        content2 = "<h1>Tomorrow's Movie</h1>"
+        content2 += "<p>" + movie2 + "</p>"
+
         self.response.write(content)
+        self.response.write(content2)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
